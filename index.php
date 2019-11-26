9375edd6f13b498c83078a4e817bbd5688fefe4c
Pijus <savickas.p@gmail.com>
2019-11-26 13:16:19 +0200

    sookie

--- index.php
+++ index.php
@@ -1,6 +1,74 @@
 <?php
+function dnd($var)
+{
+    print '<pre>';
+    print_r($var);
+    print '</pre>';
+}
 
-include 'includes/autoloader.php';
+if (isset($_POST['equal'])) {
+    if ($_POST['action'] === '+') {
+        $sum = $_POST['numb1'] + $_POST['numb2'];
+    } elseif ($_POST['action'] === '-') {
+        $sum = $_POST['numb1'] - $_POST['numb2'];
+    } elseif ($_POST['action'] === '*') {
+        $sum = $_POST['numb1'] * $_POST['numb2'];
+    } elseif ($_POST['action'] === '/') {
+        $sum = $_POST['numb1'] / $_POST['numb2'];
+    } else {
+        die('ivyko klaida');
+    }
+    $saveAction = $_POST['numb1'] . $_POST['action'] . $_POST['numb2'] . '=' . $sum;
 
-$obj = new calander\WeekDay('Mon', 400);
-$obj->afterKDays();
+    if (isset($_COOKIE['calc'])) {
+        $count = count($_COOKIE['calc']);
+    }else {
+        $count = 0;
+    }
+
+    setcookie("calc[$count]", "$saveAction", time() + 60);
+    header('location:index.php');
+}
+?>
+<html>
+<head>
+    <title>Cookie</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        section {
+            width: 100vw;
+            height: 100vh;
+            background-color: chartreuse;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+    </style>
+</head>
+<body>
+<section>
+    <div>
+        <form method="post">
+            <input name="numb1" type="number">
+            <select name="action">
+                <option value="+">sum</option>
+                <option value="-">subtract</option>
+                <option value="*">multiply</option>
+                <option value="/">devide</option>
+            </select>
+            <input name="numb2" type="number">
+            <input name="equal" type="submit" value=" = ">
+        </form>
+    </div>
+    <div>
+
+        <?php dnd($_COOKIE['calc']) ?>
+    </div>
+</section>
+</body>
+</html>
